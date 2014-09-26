431eeefdd38eee08f1015c6438430d6e1f9047c4
Izaak Meckler <ihmeckler@gmail.com>
2014-09-25 21:06:56 -0500

    Revert "Merge branch 'master' of github.com:imeckler/proof into gh-pages"
    This reverts commit 4cf0d685cb1c3895e080cdabc72bc579b4352542, reversing
    changes made to 51739eb14ecb67618389e15796820916df19a0b9.

--- Utils.hs
+++
@@ -1,6 +0,0 @@
-module Utils where
-
-import Data.Char
-
-stripRight :: String -> String
-stripRight = reverse . dropWhile isSpace . reverse
--- vim/ftdetect/proof.vim
+++
@@ -1,1 +0,0 @@
-autocmd BufNewFile,BufReadPost *.proof set filetype=proof
